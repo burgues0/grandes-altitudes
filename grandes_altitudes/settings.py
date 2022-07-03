8b10d2f1ddfd1beea3db4d30ec49411ee64e43ed
burgues0 <victordpp@outlook.com>
2022-07-03 12:25:36 -0300

    feat: add decouple library

--- grandes_altitudes/settings.py
+++ grandes_altitudes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!1kl1fmn$1$#ibd29&_l%mhf2oo9u2b9+a68=ad@58^-_tg=a8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
