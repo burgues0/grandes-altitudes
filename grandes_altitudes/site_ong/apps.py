785bb641f46850da77dc8a31093df66c6ba86854
burgues0 <victordpp@outlook.com>
2022-07-03 02:33:15 -0300

    add .gitignore

--- grandes_altitudes/site_ong/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteOngConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_ong'
---
+++ site_ong/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def index(request):
+    pass
+
+def about(request):
+    pass
+
+def subjects(request):
+    pass
