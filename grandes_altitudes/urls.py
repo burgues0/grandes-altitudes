75c4d2801ade4c7852c6c58f4e87be6f4e0585cc
burgues0 <victordpp@outlook.com>
2022-07-05 19:23:43 -0300

    feat: begin register.html

--- grandes_altitudes/urls.py
+++ grandes_altitudes/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("site_ong.urls")),
+    path("register/", include("site_ong.urls")),
 ]
--- site_ong/urls.py
+++ site_ong/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("about", views.about, name="about"),
-    #path("subjects", views.subjects, name="subjects")
+    path("register", views.register, name="register"),
 ]
