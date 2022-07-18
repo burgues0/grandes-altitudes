3e152c974387db3dfb338c5340fa3a7b78833ed5
burgues0 <victordpp@outlook.com>
2022-07-18 20:15:16 -0300

    fix: edit navbar

--- site_ong/urls.py
+++ site_ong/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup", views.signup, name="signup"),
+    path("login", views.login, name="login"),
+    # path("materias", views.materias, name="materias"),
+    # path("signup", views.signup, name="signup"),
+    # path("signup", views.signup, name="signup"),
+    # path("signup", views.signup, name="signup"),
+    # path("signup", views.signup, name="signup"),
 ]
--- site_ong/views.py
+++ site_ong/views.py
@@ -10,5 +10,5 @@ def index(request):
 def signup(request):
     return render(request, "signup.html")
 
-def subjects(request):
+def login(request):
     pass
