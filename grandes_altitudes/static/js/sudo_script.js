4526a16e6a71cbdf2ae551f0110f66bdbf401c35
burgues0 <victordpp@outlook.com>
2022-07-31 22:50:02 -0300

    feat: dropdown button js func

--- grandes_altitudes/static/js/sudo_script.js
+++ grandes_altitudes/static/js/sudo_script.js
@@ -1,1 +1,15 @@
 /* caso a gente queira colocar algum js especifico para sobescrever o do bootstrap */
+const primaryNav = document.querySelector(".nav_links");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+    if(visibility === "false"){
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }
+    if(visibility === "true"){
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
