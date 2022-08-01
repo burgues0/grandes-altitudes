e3b6b2c58b89d7b4a4936af99dede39154f81900
burgues0 <victordpp@outlook.com>
2022-07-31 23:24:14 -0300

    feat: navbar kind of complete

--- grandes_altitudes/static/js/sudo_script.js
+++ grandes_altitudes/static/js/sudo_script.js
@@ -1,15 +1,18 @@
 /* caso a gente queira colocar algum js especifico para sobescrever o do bootstrap */
 const primaryNav = document.querySelector(".nav_links");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navSignIn = document.querySelector(".redir_signup");
 
 navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
     if(visibility === "false"){
         primaryNav.setAttribute("data-visible", true);
+        navSignIn.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
     }
     if(visibility === "true"){
         primaryNav.setAttribute("data-visible", false);
+        navSignIn.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
 });
