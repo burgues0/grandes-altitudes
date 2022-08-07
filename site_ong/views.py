bd351017d7775e8a4f25359aa81687dd8fec970a
burgues0 <victordpp@outlook.com>
2022-08-07 11:32:38 -0300

    feat: fetch last 3 reviews on views.py

--- site_ong/views.py
+++ site_ong/views.py
@@ -1,10 +1,12 @@
+from datetime import date
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    last_three_reviews = Reviews.objects.all().order_by('id_review').reverse()[:3]
+    return render(request, "index.html", {"reviews" : last_three_reviews})
 
 def signup(request):
     return render(request, "signup.html")
