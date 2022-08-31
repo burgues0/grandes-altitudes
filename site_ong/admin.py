abf66e6fbd7be6b01d957ef4fd964854abf70a15
burgues0 <victordpp@outlook.com>
2022-08-31 17:29:27 -0300

    feat: begin authentication backend

--- site_ong/admin.py
+++ site_ong/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from site_ong.models import UserLogin, Coordinator, Volunteer, Materials, Reviews
+from site_ong.models import UserCredentials, Coordinator, Volunteer, Materials, Reviews
 
 # Register your models here.
 
-admin.site.register(UserLogin)
+admin.site.register(UserCredentials)
 admin.site.register(Coordinator)
 admin.site.register(Volunteer)
 admin.site.register(Materials)
