d97a0da397b47f19b326c2aa6647d4fdab58363e
burgues0 <victordpp@outlook.com>
2022-10-08 00:03:49 -0300

    feat: add whiteNoise middleware

--- grandes_altitudes/settings.py
+++ grandes_altitudes/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = config("STATIC_URL")
 STATIC_ROOT = os.path.join(BASE_DIR, config("STATIC_URL"))
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
