55ac4da935046c6edd017376566176efc6b714ee
burgues0 <victordpp@outlook.com>
2022-10-07 21:17:52 -0300

    fix: update settings static_root

--- grandes_altitudes/settings.py
+++ grandes_altitudes/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = config("STATIC_URL")
-STATIC_ROOT = (os.path.join(BASE_DIR, config("STATIC_URL")),)
+STATIC_ROOT = os.path.join(BASE_DIR, config("STATIC_URL"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
