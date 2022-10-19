f74be816e23ef3645534849ba42b33176402246d
victor-kaua <victorkauamartinsnun@gmail.com>
2022-10-19 19:23:49 -0300

    Novas materias, agr ta completin igual o bomba patch.100 % atualizado. Versiculo 8:9 Pedro.

--- site_ong/urls.py
+++ site_ong/urls.py
@@ -15,4 +15,15 @@ urlpatterns = [
     path("subjects/quimica", views.subj_qui, name="quimica"),
     path("subjects/fisica", views.subj_fis, name="fisica"),
     path("subjects/biologia", views.subj_bio, name="biologia"),
+
+
+    path("subjects/geografia", views.subj_bio, name="geografia"),
+    path("subjects/historia", views.subj_bio, name="historia"),
+    path("subjects/ingles", views.subj_bio, name="ingles"),
+    path("subjects/literatura", views.subj_bio, name="literatura"),
+    path("subjects/espanhol", views.subj_bio, name="espanhol"),
+    path("subjects/biologia", views.subj_bio, name="biologia"),
+    path("subjects/filosofia", views.subj_bio, name="filosofia"),
+    path("subjects/sociologia", views.subj_bio, name="sociologia"),
+    path("subjects/artes", views.subj_bio, name="artes"),
 ]
